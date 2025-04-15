71cc356edf26d11c8c570b396a85ba85fa032ec6
Nawal024 <nawalalzubaidi4@gmail.com>
2025-04-15 18:59:22 +0300

    dep

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ from utils import create_default_categories_if_empty
 create_default_categories_if_empty()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+ 
